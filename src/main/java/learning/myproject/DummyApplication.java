a2d19be7bd7ce1fc5d8b99e31a1c3aea31aa7699
vinodaws2020 <vinodaws2020@gmail.com>
2025-04-27 05:45:26 +0530

    src added

---
+++ src/main/java/learning/myproject/DummyApplication.java
@@ -0,0 +1,14 @@
+package learning.myproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyApplication {
+
+    public static void main(String args[]) {
+    while (true) {
+      System.out.println("Hello World");
+    }
+  }
+}
